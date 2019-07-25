87c6b6669cf7932daf5942f39ae9c93af3837f23
Syam Ahmad <syammehar@hotmail.com>
2019-07-25 16:35:34 +0500

    changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
-import { MDBContainer } from "mdbreact";
+import { MDBContainer, MDBNotification } from "mdbreact";
 import NavbarPage from "./components/NavbarPage";
 import { ControlledTabs } from "./components/ControlledTabs";
+import { Toast, Button } from "react-bootstrap";
+import Pusher from "pusher-js";
 
 export class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "Message",
+      message: "",
+      showNotification: false
+    };
+  }
+
+  componentDidMount() {
+    const pusher = new Pusher("0295f0431590b6afe528", {
+      cluster: "ap2",
+      encrypted: true
+    });
+    const channel = pusher.subscribe("my-channel");
+    channel.bind("my-event", data => {
+      //this.setState({ message: data.message, showNotification: true });
+      //this functon is trigerd whenever a notification is send from server through pusher.
+      console.log(this.state);
+    });
+  }
   render() {
     return (
-      <MDBContainer>
-        <NavbarPage />
-        <ControlledTabs />
-      </MDBContainer>
+      <React.Fragment>
+        <MDBContainer>
+          <NavbarPage />
+          <ControlledTabs />
+        </MDBContainer>
+        {
+          //Notificatin toast to be placed here..........................
+        }
+      </React.Fragment>
     );
   }
 }
